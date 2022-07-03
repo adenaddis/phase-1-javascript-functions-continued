1ba63d3570dba356d04dacb4b399f81998e623ae
Aden Addis <adenwonde56@gmail.com>
2022-07-02 21:49:15 -0400

    commit tree 21d3503cc4bc1798ae7467749f6a1a5a71c19876

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+// function saturdayFun() {
+//     console.log("roller-skate")
+// }
+// // saturdayFun()
+
+// saturdayFun("This Saturday, I want to roller-skate!")
+
+// function saturdayFun(sent= "This Saturday, I want to", action = "roller-skate") {
+//     console.log('${sent} ${action'})
+// }
+// function saturdayFun (action = "roller-skate") {
+//     console.log(saturdayFun)
+// }
+// console.log('This Saturday, I want to ${action}!')
+
+// function saturdayFun( action = "roller-skate") {
+//     console.log('This Saturday, I want to ${action}! ')
+// }
+
+// saturdayFun()
+
+function saturdayFun( activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+
